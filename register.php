304dca74e177a042b17cc19e5ba214d65d969645
jszama <jmszamotulski@gmail.com>
2023-02-20 16:38:41 +0000

    Registration page complete.

--- register.php
+++ register.php
@@ -16,11 +16,27 @@
       </style> <?php
   } ?>
   
+  <script>
+  function toggleEmployer() {
+    document.getElementById('Email').disabled = true;
+    document.getElementById('Number').disabled = true;
+    document.getElementById('Organisation').disabled = false;
+  }
+
+  function toggleJobseeker() {
+    document.getElementById('Email').disabled = false;
+    document.getElementById('Number').disabled = false;
+    document.getElementById('Organisation').disabled = true;
+  }
+
+  </script>
+
 </head>
 <body>
 <div class="header">
   <h1>JakubJobs</h1>
 </div>
+<div>
 <form action="" method="POST">
   <label>Username:</label><br>
   <input type='text' name='Username' value=""/><br>
@@ -29,20 +45,21 @@
   <input type='text' name='Password' value=""/><br>
 
   <label>Type:</label><br>
-  <input type='radio' name='Type' value="Job-seeker"/>Job-seeker
-  <input type='radio' name='Type' value="Employer"/>Employer<br>
+  <input type='radio' name='Type' value="Job-seeker" onclick="toggleJobseeker()"/>Job-seeker
+  <input type='radio' name='Type' value="Employer" onclick="toggleEmployer()"/>Employer<br>
 
   <label>Email:</label><br>
-  <input type='text' name='Email' value=""/><br>
+  <input type='text' id='Email' name='Email' value=""/><br>
 
   <label>Number:</label><br>
-  <input type='text' name='Number' value=""/><br>
+  <input type='text' id='Number' name='Number' value=""/><br>
 
   <label>Organisation:</label><br>
-  <input type='text' name='Organisation' value=""/><br>
+  <input type='text' id='Organisation' name='Organisation' value=""/><br>
   <p class="errors"><?php echo $error; ?> <?php echo $success; ?></p>
 
   <input type="submit" name="register" value="Register"/>
 </form>
+</div>
 </body>
 </html>
--- validateRegister.php
+++ validateRegister.php
@@ -15,29 +15,45 @@ $conn = new mysqli($DBservername, $DBusername, $DBpassword, $DBdatabase);
 if(isset($_POST['register'])){
   $_username = $_POST['Username'];
   $_password = $_POST['Password'];
-  $_type = $_POST['Type'];
-  $_email = $_POST['Email'];
-  $_number = $_POST['Number'];
+  if (isset($_POST['Type'])){
+    $_type = $_POST['Type'];
+  }
+  if (isset($_POST['Email'])){
+    $_email = $_POST['Email'];
+  }
+  if (isset($_POST['Number'])){
+    $_number = $_POST['Number'];
+  }
   $_organisation = $_POST['Organisation'];
 
-  $sql1 = "SELECT username FROM Account WHERE username='$_username';";
-  $result = $conn->query($sql);
+  $query = "SELECT username FROM Account WHERE username='$_username';";
+  $result = $conn->query($query);
 
-  if(empty(trim($_username)) or (5>= strlen($_username) <= 20) or  $result->num_rows == 0){
+  if(empty(trim($_username)) or (5 >= strlen($_username) and strlen($_username) <= 20) or  $result->num_rows != 0){
     $error = "Invalid username, length(5-20).";
-  } elseif (empty(trim($_password)) or (6>= strlen($_password) <= 20)){
+  } elseif (empty(trim($_password)) or (6 >= strlen($_password) and strlen($_password) <= 20)){
     $error = "Invalid password, length (5-20).";
   } elseif (empty($_type)) {
     $error = "Empty type.";
-  } elseif (($_type = "Job-seeker") and str_contains($_email, '@') and str_contains($_email, ',')){
-    $error = "Invalid email.";
-  } elseif (($_type = "Job-seeker") and (strlen($_number) != 11)) {
+  } elseif (($_type == "Job-seeker") and (str_contains($_email, '@') == False or str_contains($_email, '.') == False)){
+    $error = "Invalid Email.";
+  } elseif (($_type == "Job-seeker") and (strlen($_number) != 11 or is_numeric($_number) == False)) {
     $error = "Invalid number.";
+  } elseif (($_type == "Employer") and (empty(trim($_organisation)))){
+    $error = "Invalid organisation.";
+  } else {
+    $query1 = "INSERT INTO Account (username, password, type, email, number) VALUES ('$_username', '$_password', '$_type', '$_email', '$_number');";
+    $query2 = "INSERT INTO Account (username, password, type, organisation) VALUES ('$_username', '$_password', '$_type', '$_organisation');";
+    if ($_type == "Job-seeker"){
+      mysqli_query($conn, $query1);
+      $success = "Account successfully created.";
+      header('Location:loginPage.php');
+    } elseif ($_type == "Employer") {
+      mysqli_query($conn, $query2);
+      $success = "Account successfully created.";
+      header('Location:loginPage.php');
+    }
   }
-  elseif (($_type = "Employer") and (strlen($_number) != 11)) {
-    $error = "Invalid number.";
-  }
-
 }
 
 $conn->close();
