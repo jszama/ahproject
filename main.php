7ce8826efe0aecafb2a22b806aac3c092b64e290
jszama <jmszamotulski@gmail.com>
2023-02-24 16:53:37 +0000

    Setting up homepage

---
+++ main.php
@@ -0,0 +1,38 @@
+<?php require("search.php") ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>JakubJobs</title>
+  <link href="style.css" rel="stylesheet" type="text/css"/>
+</head>
+<body>
+<div>
+<div class="header">
+  <h1>JakubJobs</h1>
+</div>
+<div>
+    <ul>
+        <li>Home</li>
+        <li>Account</li>
+    </ul>
+</div>
+</div>
+<div>
+    <div>
+        <form method="POST">
+            <input type="text" placeholder="Search" name="search">  
+        </form>
+        <form method="POST">
+            <select>
+                    <option value="">asd</option>
+                    <option value="">das</option>
+                    <option value="">fdfds</option> 
+            </select>
+            <input type="submit" name="addQuality" value="Add quality"/>
+        </form>
+    </div>
+</div>
+</body>
+</html>
