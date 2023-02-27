f2dae8dc9e5532be83e92cba639cedd9caf4d0bf
Jakub Szamotulski <jmszamotulski@gmail.com>
2023-02-27 07:31:40 +0000

    Finished search and create

--- main.php
+++ main.php
@@ -1,4 +1,5 @@
-<?php require("search.php") ?>
+<?php require("search.php")?>
+<?php session_start();?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -6,9 +7,43 @@
   <meta name="viewport" content="width=device-width">
   <title>JakubJobs</title>
   <link href="style.css" rel="stylesheet" type="text/css"/>
+  <?php
+    // Create connection
+    $DBservername = "localhost";
+    $DBdatabase = "account";
+    $DBusername = "root";
+    $DBpassword = "";
+
+    $conn = new mysqli($DBservername, $DBusername, $DBpassword, $DBdatabase);
+    $query = "SELECT * FROM Post";
+    $result = $conn->query($query);
+
+
+    if(isset($_POST['create'])){
+      if (isset($_POST['quality'])){
+        $_userQualities = $_POST['quality'];
+      }
+      $_userQualitie = implode(", ", $_userQualities);
+      $_sessionUsername = $_SESSION['Username'];
+      $_title = $_POST['Title'];
+      $query2 = "INSERT INTO `post` (`Username`, `Title`, `Qualities`, `Email`, `Number`) VALUES ('$_sessionUsername', '$_title', '$_userQualitie', '$_email', '$_number');";
+      mysqli_query($conn, $query2);
+      $result = $conn->query($query);
+    }
+  ?>
+  <script>
+    function openCreate() {
+      document.getElementById("myForm").style.display = "block";
+    }
+
+    function closeCreate() {  
+        document.getElementById("myForm").style.display = "none";
+    }
+  </script>
 </head>
 <body>
 <div>
+<?php echo $_SESSION['Username']; ?>
 <div class="header">
   <h1>JakubJobs</h1>
 </div>
@@ -22,16 +57,50 @@
 <div>
     <div>
         <form method="POST">
-            <input type="text" placeholder="Search" name="search">  
+          <input type="checkbox" name="quality[]" value="Good Communication"/>
+          <label>Good Communication</label>
+          <input type="checkbox" name="quality[]" value="Good Leadership"/>
+          <label>Good Leadership</label>
+          <input type="checkbox" name="quality[]" value="Good Problem-solving"/>
+          <label>Good Problem-solving</label>
+          <input type="submit" name="submit" value="Search"/>
         </form>
+        <button class="createButton" onclick="openCreate()">Create</button>
+    </div>
+    <div>
+      <div class="createPopup" id="myForm" style="display: none;">
         <form method="POST">
-            <select>
-                    <option value="">asd</option>
-                    <option value="">das</option>
-                    <option value="">fdfds</option> 
-            </select>
-            <input type="submit" name="addQuality" value="Add quality"/>
+          <label><b>Title</b></label>
+          <input type="text" placeholder="Enter Title" name="Title">
+
+          <label><b>Qualities</b></label>
+          <input type="checkbox" name="quality[]" value="Good Communication"/>
+          <label>Good Communication</label>
+          <input type="checkbox" name="quality[]" value="Good Leadership"/>
+          <label>Good Leadership</label>
+          <input type="checkbox" name="quality[]" value="Good Problem-solving"/>
+          <label>Good Problem-solving</label>
+
+          <input type="submit" name="create" value="Create"/>
+          <button type="button" class="button cancel" onclick="closeCreate()">Close</button>
         </form>
+      </div>
+      <div class="display">
+          <?php
+              if(isset($_POST['submit'])){
+                if (isset($_POST['quality'])){
+                  $_qualities = $_POST['quality'];
+                }
+                $query2 = "SELECT * FROM Post WHERE Qualities IN ('" . implode("', '", $_qualities) . "')";
+                $result = $conn->query($query);
+              }
+  
+              while ($row = mysqli_fetch_assoc($result)){
+                echo $row['Title'];
+                echo $row['Qualities'];
+              }
+          ?>
+      </div>
     </div>
 </div>
 </body>
--- search.php
+++ search.php
@@ -1,37 +1,6 @@
+<?php require("validateLogin.php")?>
 <?php
 // Input validation variables
 $error = null; 
 $success = null;
-
-// Create connection
-$DBservername = "localhost";
-$DBdatabase = "account";
-$DBusername = "root";
-$DBpassword = "";
-
-
-$conn = new mysqli($DBservername, $DBusername, $DBpassword, $DBdatabase);
-
-if(isset($_POST['title'])){
-  $_title = $_POST['Title'];
-  $_qualities = $_POST[''];
-
-  if(empty(trim($_username)) or empty(trim($_password))){
-      $error = "Field(s) are empty.";
-  } else {
-      // Check login detiails
-      $sql = "SELECT * FROM Account WHERE username='$_username' AND password='$_password';";
-      $result = $conn->query($sql);
-
-      if ($result->num_rows == 1) {
-          $success = 'Correct login! Redirecting';
-          header('Location:main.php');
-          $_sessionUsername = $_username;
-      } else {
-          $error = 'Incorrect login details.';
-    }
-    }
-  }
-
-$conn->close();
 ?>
